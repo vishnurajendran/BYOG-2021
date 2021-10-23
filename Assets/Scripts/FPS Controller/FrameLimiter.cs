b54e7f381d6062cf3ca4b6b134dd037f5abd9b79
Vishnu Rajendran <ssj5vishnu@gmail.com>
2021-10-23 22:10:11 +0530

    Added frame limiter script

---
+++ Assets/Scripts/FPS Controller/FrameLimiter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameLimiter : MonoBehaviour
+{
+    [SerializeField] int target = 60;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = target;
+    }
+
+}
